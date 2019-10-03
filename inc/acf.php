28fbf5d83a46d6195328ee4e4512b874b7fca83b
Tim Loden <tim.m.loden@gmail.com>
2019-10-03 12:54:08 -0700

    updated wpgulp packages

--- inc/acf.php
+++ inc/acf.php
@@ -6,7 +6,7 @@ if (function_exists('acf_add_options_page') ) {
 
     acf_add_options_page(
         array(
-        'page_title'     => '',
+        'page_title'     => 'Site Options',
         'menu_title'    => 'Site Options',
         'menu_slug'     => 'site-options',
         'capability'    => 'edit_posts',
